df898010c24548e3f2e0b9fe6a9c41be8f7b0328
UpInTheAir <upintheair.xda@gmail.com>
2015-04-22 09:12:06 +0700

    workqueue: disable PEWQ by default

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -253,7 +253,7 @@ struct workqueue_struct {
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
 #ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
-static bool wq_power_efficient = true;
+static bool wq_power_efficient = false;
 #else
 static bool wq_power_efficient;
 #endif
