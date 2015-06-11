6c265979d825c81cefe7e9da23c496db4d3cdcb6
UpInTheAir <upintheair.xda@gmail.com>
2015-06-12 06:22:37 +0700

    GPU: add GPU: voltage control
    adjusted for midgard_wk04
    
    original credits to @halaszk
    
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- drivers/gpu/arm/midgard_wk04/platform/5420/mali_kbase_dvfs.c
+++ drivers/gpu/arm/midgard_wk04/platform/5420/mali_kbase_dvfs.c
@@ -36,6 +36,7 @@
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/pm_qos.h>
+#include <linux/sysfs_helpers.h>
 
 #include <mach/bts.h>
 #include <mach/map.h>
@@ -173,6 +174,29 @@ static void update_time_in_state(int level);
 static mali_dvfs_status mali_dvfs_status_current;
 #ifdef MALI_DVFS_ASV_ENABLE
 static const unsigned int mali_dvfs_vol_default[] = { 812500, 812500, 862500, 912500, 962500, 1000000, 1037500, 1050000, 1075000};
+
+ssize_t hlpr_get_gpu_volt_table(char *buf)
+{
+       int i, len = 0;
+
+       for (i = 0; i < MALI_DVFS_STEP; i++) {
+               len += sprintf(buf + len, "%d %d\n", mali_dvfs_infotbl[i].clock, mali_dvfs_infotbl[i].voltage);
+       }
+
+       return len;
+}
+
+void hlpr_set_gpu_volt_table(int gpu_table[])
+{
+        int i;
+        int u = 0;
+        for (i = 0; i < MALI_DVFS_STEP; i++)
+        {
+                mali_dvfs_infotbl[i].voltage = gpu_table[u];
+                pr_alert("SET GPU VOLTAGE TABLE %d - %d - %d", i, mali_dvfs_infotbl[i].clock, mali_dvfs_infotbl[i].voltage);
+                u++;
+	}
+}
 
 ssize_t hlpr_get_gpu_gov_table(char *buf)
 {
